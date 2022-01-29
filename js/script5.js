20a5e648eab4e3c12b322b447b6de473ec901323
Sagynbekov Adilet <adilet.sagynbekov.kk@gmail.com>
2022-01-29 14:53:28 +0600

    fixed bugs

--- js/script5.js
+++ js/script5.js
@@ -1,6 +1,6 @@
 //Задание №5
-let number = prompt("Введите номер своей карты: ", 111111111);
-let numberHidden = number.slice(0, 5);
+let number = prompt("Введите номер своей карты: ");
+let numberHidden = number.slice(0, -4);
 let result = number.replace(numberHidden, "****");
 
 document.write("<h4>Номер вашей карты: " + result + "</h4>");
